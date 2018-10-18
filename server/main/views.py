79a3120e9b080cda0d342e036111e0a60a691c13
Castrtroy <castrtroy@gmail.com>
2018-10-18 11:17:38 +0300

    settings.py add ALLOWED_HOSTS

---
+++ server/main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def main(request):
+    return render(request, 'main/index.html')
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
