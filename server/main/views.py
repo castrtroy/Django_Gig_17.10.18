4793342df930062ea9e223dda3e268026f462461
Castrtroy <castrtroy@gmail.com>
2018-10-18 14:03:47 +0300

    add page1

--- server/main/views.py
+++ server/main/views.py
@@ -4,10 +4,11 @@ from django.shortcuts import render
 def main(request):
     return render(request, 'main/index.html')
 
-
 def contacts(request):
     return render(request, 'main/contacts.html')
 
-
 def about(request):
     return render(request, 'main/about.html')
+
+def page1(request):
+    return render(request, 'main/page1.html')
