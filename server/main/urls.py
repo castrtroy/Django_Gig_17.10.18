f3b653ed379902d6734afcece87def7a71a1fe42
Castrtroy <castrtroy@gmail.com>
2018-10-18 11:17:38 +0300

    settings.py add ALLOWED_HOSTS

---
+++ server/main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from main.views import (
+    main, contacts, about
+)
+
+
+urlpatterns = [
+    path('contacts/', contacts),
+    path('about/', about),
+    path('', main),
+]
