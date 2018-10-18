77c5e4828baa0072a2f696f3101c13bb9cfce31e
Castrtroy <castrtroy@gmail.com>
2018-10-18 15:39:06 +0300

    2

--- server/main/urls.py
+++ server/main/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from main.views import (
-    main, contacts, about
+    main, contacts, about, page1
 )
 
 
 urlpatterns = [
     path('contacts/', contacts),
     path('about/', about),
+    path('page1/', page1),
     path('', main),
+    
 ]
